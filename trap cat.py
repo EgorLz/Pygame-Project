f41f375fa4df1905f084e6f210446f47a66972f6
EgorLz <mikhaylov2013A@yandex.ru>
2021-12-29 22:10:11 +0300

    начало работы

---
+++ trap cat.py
@@ -0,0 +1,21 @@
+import os
+import sys
+
+import pygame
+
+pygame.init()
+size = width, height = 500, 500
+screen = pygame.display.set_mode(size)
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    return image
+
+image = load_image("pole.png")
+
+all_sprites = pygame.sprite.Group()
