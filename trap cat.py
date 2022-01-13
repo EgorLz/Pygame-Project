db6c7a7904b740a9637021df6b2eb342d85b23a7
Student <mikahylov2013a@yandex.ru>
2022-01-13 18:44:40 +0300

    Esc add

--- trap cat.py
+++ trap cat.py
@@ -83,6 +83,9 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False
         all_sprites.update(event)
     all_sprites.draw(screen)
     pygame.display.flip()
