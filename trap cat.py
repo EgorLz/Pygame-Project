d2baf4d0e33f99682487f50edbb1d1f2359a7409
Student <mikhaylov2013a@yandex.ru>
2022-02-10 19:00:17 +0300

    xs

--- trap cat.py
+++ trap cat.py
@@ -18,29 +18,29 @@ pygame.display.set_caption('Trap cat by lazzzy')
 
 manager = pygame_gui.UIManager((970, 500))
 
-start_btn = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((435, 300), (100, 50)),
-                                            text='Start',
-                                            manager=manager)
-
-diifculti1 = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((350, 200), (50, 50)),
-                                            text='1',
-                                            manager=manager)
-
-diifculti2 = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((400, 200), (50, 50)),
-                                            text='2',
-                                            manager=manager)
-
-diifculti3 = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((450, 200), (50, 50)),
-                                            text='3',
-                                            manager=manager)
-
-diifculti4 = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((500, 200), (50, 50)),
-                                            text='4',
-                                            manager=manager)
-
-diifculti5 = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((550, 200), (50, 50)),
-                                            text='5',
-                                            manager=manager)
+# start_btn = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((435, 300), (100, 50)),
+#                                             text='Start',
+#                                             manager=manager)
+#
+# diifculti1 = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((350, 200), (50, 50)),
+#                                             text='1',
+#                                             manager=manager)
+#
+# diifculti2 = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((400, 200), (50, 50)),
+#                                             text='2',
+#                                             manager=manager)
+#
+# diifculti3 = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((450, 200), (50, 50)),
+#                                             text='3',
+#                                             manager=manager)
+#
+# diifculti4 = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((500, 200), (50, 50)),
+#                                             text='4',
+#                                             manager=manager)
+#
+# diifculti5 = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((550, 200), (50, 50)),
+#                                             text='5',
+#                                             manager=manager)
 
 
 def load_image(name, colorkey=None):
@@ -86,6 +86,7 @@ def start_screen():
 
 
 all_sprites = pygame.sprite.Group()
+hero = pygame.sprite.Group()
 board = [[0] * 11 for _ in range(11)]
 
 
@@ -109,6 +110,7 @@ class Pole(pygame.sprite.Sprite):
             if time.time() - start_time >= 0.7:
                 self.image = Pole.pole_close
                 start_time = time.time()
+                board[pos_x][pos_y] = 1
 
 
 class Cat(pygame.sprite.Sprite):
@@ -123,12 +125,40 @@ class Cat(pygame.sprite.Sprite):
         self.pos_x = pos_x
         self.pos_y = pos_y
 
-    def check_pole(self):
+    def check_pole_l(self):
         if board[self.pos_y][self.pos_x - 1] == 0:
-            return 0
+            return True
         else:
-            return 1
+            return False
 
+    def check_pole_lh(self):
+        if board[self.pos_y - 1][self.pos_x] == 0:
+            return True
+        else:
+            return False
+
+    def check_pole_ll(self):
+        if board[self.pos_y + 1][self.pos_x] == 0:
+            return True
+        else:
+            return False
+
+    def move_l(self):
+        self.rect.x -= 85
+        self.pos_x -= 1
+
+    def move_lh(self):
+        self.pos_y -= 1
+
+    def move_ll(self):
+        self.pos_y += 1
+
+    def update(self):
+        if self.check_pole_l():
+            self.move_l()
+
+
+n = 8
 def drawpole(n):
     for i in range(11):
         for j in range(11):
@@ -156,42 +186,50 @@ pos_x = 430
 pos_y = 190
 cat_x = 6
 cat_y = 6
-Cat(pos_x, pos_y, cat_x, cat_y, all_sprites)
 
 start_time = time.time()
 
 clock = pygame.time.Clock()
-running = True
+def main():
+    running = True
 
-start_screen()
-while running:
-    time_delta = clock.tick(60) / 1000.0
-    screen.fill('white')
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
+    start_screen()
+    drawpole(n)
+    cat = Cat(pos_x, pos_y, cat_x, cat_y, hero)
+    while running:
+        time_delta = clock.tick(60) / 1000.0
+        screen.fill('white')
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
                 running = False
-        if event.type == pygame_gui.UI_BUTTON_PRESSED:
-            n = 8
-            if event.ui_element == diifculti1:
-                n = 10
-            if event.ui_element == diifculti2:
-                n = 8
-            if event.ui_element == diifculti3:
-                n = 6
-            if event.ui_element == diifculti4:
-                n = 5
-            if event.ui_element == diifculti5:
-                n = 4
-            if event.ui_element == start_btn:
-                drawpole(n)
-        manager.process_events(event)
-        all_sprites.update(event)
-    all_sprites.draw(screen)
-    manager.update(time_delta)
-    screen.blit(background, (0, 0))
-    manager.draw_ui(screen)
-    pygame.display.flip()
-pygame.quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    running = False
+            # if event.type == pygame_gui.UI_BUTTON_PRESSED:
+            #     n = 8
+            #     if event.ui_element == diifculti1:
+            #         n = 10
+            #     if event.ui_element == diifculti2:
+            #         n = 8
+            #     if event.ui_element == diifculti3:
+            #         n = 6
+            #     if event.ui_element == diifculti4:
+            #         n = 5
+            #     if event.ui_element == diifculti5:
+            #         n = 4
+            #     if event.ui_element == start_btn:
+            #         drawpole(n)
+            manager.process_events(event)
+            all_sprites.update()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                hero.update()
+        all_sprites.draw(screen)
+        hero.draw(screen)
+        manager.update(time_delta)
+        # # screen.blit(background, (0, 0))
+        # # manager.draw_ui(screen)
+        pygame.display.flip()
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
